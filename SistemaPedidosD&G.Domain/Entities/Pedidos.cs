3c8aec0a70abd10c6bdbf5bb2dcb6551bbbe1c80
GabrielRuizEespinosa14 <gabrielmoisesruizespinosa@gmail.com>
2026-07-17 01:02:18 -0400

    Estructura inicial del sistema de pedidos

---
+++ SistemaPedidosD&G.Domain/Entities/Pedidos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidosD_G.Domain.Entities
+{
+    public class Pedidos
+    {
+        //Propiedades
+        public Guid Id { get; private set; }
+        public string CodigoPedido { get; private set; } = null!;
+        public string Nombre { get; private set; } = null!;
+        public string Direccion { get; private set; } = null!;
+        //Propiedades de navegación
+        //Constructores
+        public Pedidos (string codigoPedido, string nombre, string direccion)
+        {
+            Id = Guid.NewGuid();
+            CodigoPedido = codigoPedido;
+            Nombre = nombre;
+            Direccion = direccion;
+        }
+        //Métodosss
+    }
+}
